47fa78076adf270090515291b0220232ce8ca7f8
d06hara <d06.harada.prg@gmail.com>
2023-10-30 23:40:04 +0900

    typescrit環境構築

--- nvchad/custom/configs/lspconfig.lua
+++ nvchad/custom/configs/lspconfig.lua
@@ -1,10 +1,36 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 local util = require "lspconfig.util"
 
+local servers = { "html", "cssls", "jsonls", "unocss", "emmet_language_server", "gopls", "tsserver" }
 
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+local custom_on_attach = function(client, bufnr)
+  on_attach(client, bufnr)
+
+  if client.server_capabilities.inlayHintProvider then
+    vim.lsp.inlay_hint(bufnr, true)
+  end
+end
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = custom_on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- go --
 lspconfig.gopls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
@@ -20,4 +46,29 @@ lspconfig.gopls.setup {
       }
     }
   }
+}
+
+-- typescript --
+-- lspconfig.tsserver.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   init_options = {
+--     preferences = {
+--       disableSuggestions = true
+--     }
+--   },
+--   commands = {
+--     OrganizeImports = {
+--       organize_imports,
+--       description = "Organize Imports"
+--     }
+--   }
+-- }
+
+require("typescript-tools").setup {
+  on_attach = custom_on_attach,
+
+  settings = {
+    tsserver_path = "/home/sid/.volta/tools/shared/typescript/lib/tsserver.js",
+  },
 }
--- nvchad/custom/configs/overrides.lua
+++ nvchad/custom/configs/overrides.lua
@@ -1,31 +1,85 @@
+-- overriding default plugin configs!
+
 local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "html",
+    "css",
+    "javascript",
+    "json",
+    "toml",
+    "markdown",
+    "c",
+    "bash",
+    "lua",
+    "tsx",
+    "typescript",
+  },
+}
+
+M.nvimtree = {
+  filters = {
+    dotfiles = true,
+    custom = { "node_modules" },
+  },
+  git = {
+    enable = true,
+    ignore = true,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
 
 M.mason = {
   ensure_installed = {
     -- lua stuff
     "lua-language-server",
-    "stylua",
+    -- "stylua",
 
     -- web dev
     "css-lsp",
     "html-lsp",
-    -- "typescript-language-server",
-    "deno",
-    "emmet-ls",
+    "typescript-language-server",
+    "biome",
+    "eslint-lsp",
+    -- "deno",
+    "prettier",
+    "js-debug-adapter",
+    -- "emmet-ls",
     "json-lsp",
     -- "tailwindcss-language-server",
-    "unocss-language-server",
-
-    -- server
-    "gopls",
+    -- "unocss-language-server",
 
     -- shell
-    "shfmt",
-    "shellcheck",
-    "bash-language-server",
+    -- "shfmt",
+    -- "shellcheck",
+    -- "bash-language-server",
 
     -- "clangd",
     -- "clang-format",
+  },
+}
+
+M.cmp = {
+  sources = {
+    -- trigger_characters is for unocss lsp
+    { name = "nvim_lsp", trigger_characters = { "-" } },
+    { name = "path" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "codeium" },
+    { name = "nvim_lua" },
+  },
+  experimental = {
+    ghost_text = true,
   },
 }
 
